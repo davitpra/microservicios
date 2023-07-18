62264213ba7be1f9853e85dcb91c9d32856ab098
David <j.david.prado1716@gmail.com>
2023-07-18 10:54:48 -0500

    autenticacion: gestion de permisos

---
+++ api/components/user/secure.js
@@ -0,0 +1,21 @@
+const auth = require('../../../auth');
+
+// middleware para checkear si es usuario es validado.
+module.exports = function checkAuth(action) {
+
+    function middleware(req, res, next) {
+        // dependiendo la accion que queramos validar
+        switch(action) {
+            case 'update':
+                const owner = req.body.id;
+                // aqui comprobamos que el usuario sea el mismo
+                auth.check.own(req, owner);
+                break;
+
+            default:
+                next();
+        }
+    }
+
+    return middleware;
+}
--- auth/index.js
+++ auth/index.js
@@ -1,8 +1,56 @@
 const jwt = require('jsonwebtoken');
+const config = require('../config');
 
+const secret = config.jwt.secret;
+
+// funcion para firmar un token
 function sign(data) {
     // firmamos el token con nuestro secreto.
-    return jwt.sign(data, 'secreto');
+    return jwt.sign(data, secret);
+}
+
+
+
+// funcion para checkear el token.
+const check = {
+    own: function(req, owner) {
+        // decodificamos el token
+        const decoded = decodeHeader(req)
+        console.log(decoded)
+    },
+}
+
+// para decodificar el token del header y verificar
+function decodeHeader(req) {
+    // guardamos la autorizacion del header
+    const authorization = req.headers.authorization || '';
+    //obtenemos el token de la cabezera
+    const token = getToken(authorization);
+    //decodificamos el token del
+    const decoded = verify(token);
+    //guardamos en el request
+    req.user = decoded;
+    // devolvemos la verificacion true o false
+    return decoded;
+}
+
+// para obtener el token del header
+function getToken(auth) {
+    if (!auth) {
+        throw new Error('No viene token');
+    }
+
+    if (auth.indexOf('Bearer ') === -1) {
+        throw new Error('Formato invalido');
+    }
+
+    let token = auth.replace('Bearer ', '');
+    return token;
+}
+
+//funcion para verificar si el token es nuestro 
+function verify(token) {
+    return jwt.verify(token, secret)
 }
 
 module.exports = {
