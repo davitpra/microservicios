2509949cedf98cc1d06abf48bd2833cc82e6c580
David <j.david.prado1716@gmail.com>
2023-07-19 23:13:39 -0500

    Separando los posts a un microservicio

--- api/index.js
+++ api/index.js
@@ -6,7 +6,6 @@ const swaggerUi = require('swagger-ui-express')
 const config = require('../config.js')
 const user = require('./components/user/network')
 const auth = require('./components/auth/network')
-const post = require('./components/post/network');
 const errors = require('../network/errors.js')
 
 const app = express()
@@ -21,7 +20,6 @@ const swaggerDoc = require('./swagger.json')
 //Router
 app.use('/api/user', user)
 app.use('/api/auth', auth)
-app.use('/api/post', post);
 // ruta para ver la documentacion con swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 
